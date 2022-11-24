c32a64472fbbf8c387076ab1d82932a706b50a09
Théo Prigent <dev@tprigent.fr>
2022-11-24 20:01:22 +0000

    Enriched move primitives for the robot

--- robot.py
+++ robot.py
@@ -3,29 +3,49 @@ import tools
 
 
 class Point:
-    def __init__(self):
-        self.name = 'pos'
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.p = 0
-        self.r = 0
+    def __init__(self, name='point', x=0, y=0, z=0, p=0, r=0):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.z = z
+        self.p = p
+        self.r = r
+
+    def print_point(self):
+        print('\n### POINT {} coordinates ###'.format(self.name))
+        print('X={}  Y={}  Z={}  P={}  Z={}'.format(self.x, self.y, self.z, self.p, self.r))
+
+
+# create a point in robot's memory
+def init_point(ser, name):
+    tools.send(ser, 'defp {}'.format(name))
+    tools.send(ser, 'here {}'.format(name))
+    return Point(name=name)
+
+
+# fills Point instance with robot memory data
+def get_point_coordinates(ser, point):
+    response = tools.send(ser, 'listpv {}'.format(point.name))
+    # todo: parse response to fill point structure
 
 
 # function that change the coordinates x,y,z,p,r of a position pos relatively to P0
 # '{}'.format(x) permet d'envoyer une valeur à la place de la variable au lieu d'un caractère
-def change(ser, pos, p0, x, y, z, p, r):
-    tools.send(ser, 'teach {}'.format(pos))
-    if x is not None: tools.send(ser, 'setpvc {} X {}'.format(pos, p0.x + x))
-    if y is not None: tools.send(ser, 'setpvc {} Y {}'.format(pos, p0.y + y))
-    if z is not None: tools.send(ser, 'setpvc {} Z {}'.format(pos, p0.z + z))
-    if p is not None: tools.send(ser, 'setpvc {} P {}'.format(pos, p0.p + p))
-    if r is not None: tools.send(ser, 'setpvc {} R {}'.format(pos, p0.r + r))
+def set_point_coordinates(ser, point, p0, x=None, y=None, z=None, p=None, r=None):
+    if x is not None: tools.send(ser, 'setpvc {} X {}'.format(point.name, p0.x + x))
+    if y is not None: tools.send(ser, 'setpvc {} Y {}'.format(point.name, p0.y + y))
+    if z is not None: tools.send(ser, 'setpvc {} Z {}'.format(point.name, p0.z + z))
+    if p is not None: tools.send(ser, 'setpvc {} P {}'.format(point.name, p0.p + p))
+    if r is not None: tools.send(ser, 'setpvc {} R {}'.format(point.name, p0.r + r))
 
 
-# function that move up and down relatively to P0
-def move_pen(ser, p0, pos, up):
+# function that move up and down pen at a given point relatively to P0
+# todo: check if point has to be moved or just direct command
+#           => modifying point coordinates may imply problem in memorized points
+def moveup_pen(ser, p0, point, up):
     if up is True:
-        change(ser=ser, pos=pos, p0=p0, x=None, y=None, z=p0.z + 10, p=None, r=None)
+        set_point_coordinates(ser=ser, point=point, p0=p0, z=10)
+        tools.send(ser, 'move {}'.format(point.name))
     else:
-        change(ser=ser, pos=pos, p0=p0, x=None, y=None, z=p0.z - 10, p=None, r=None)
+        set_point_coordinates(ser=ser, point=point, p0=p0, z=-10)
+        tools.send(ser, 'move {}'.format(point.name))
