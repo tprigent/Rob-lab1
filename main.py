3f572596584a648831524ac46c02ef4bf80b9cce
Théo Prigent <dev@tprigent.fr>
2022-11-28 15:11:58 +0000

    Rollback: undo changes in main

--- main.py
+++ main.py
@@ -7,32 +7,29 @@ import acquisition
 
 
 if __name__ == '__main__':
-    robot.get_point_coordinates(point=robot.Point())
-    #acquisition.get_key_points('letters.png', 20)
+    ser = serial_tools.connect_serial('COM3')
 
-    # ser = serial_tools.connect_serial('COM3')
-    #
-    # if ser is not None:
-    #     print('\n### BEGIN SEQUENCE ###')
-    #     # Draw a flat square from given p0
-    #     serial_tools.send(ser, 'SPEED 20')
-    #     serial_tools.send(ser, 'DIMP square[4]')
-    #     serial_tools.send(ser, 'HERE square[1]')
-    #     serial_tools.send(ser, 'HERE square[2]')
-    #     serial_tools.send(ser, 'SETPVC square[2] X 200')
-    #     serial_tools.send(ser, 'HERE square[3]')
-    #     serial_tools.send(ser, 'SETPVC square[2] Y 300')
-    #     serial_tools.send(ser, 'HERE square[4]')
-    #     serial_tools.send(ser, 'SETPVC square[4] X 200')
-    #     serial_tools.send(ser, 'SETPVC square[4] Y 300')
-    #
-    #     for i in range(4):
-    #         msg1 = 'TEACH square[{}]'.format(i)
-    #         serial_tools.send(ser, msg1)
-    #         msg2 = 'MOVE square[{}]'.format(i)
-    #         serial_tools.send(ser, msg2)
-    #         msg3 = 'HERE square[{}]'.format(i)
-    #         serial_tools.send(ser, msg3)
-    #
-    #     serial_tools.send(ser, 'MOVES square 1 4')
+    if ser is not None:
+        print('\n### BEGIN SEQUENCE ###')
+        # Draw a flat square from given p0
+        serial_tools.send(ser, 'SPEED 20')
+        serial_tools.send(ser, 'DIMP square[4]')
+        serial_tools.send(ser, 'HERE square[1]')
+        serial_tools.send(ser, 'HERE square[2]')
+        serial_tools.send(ser, 'SETPVC square[2] X 200')
+        serial_tools.send(ser, 'HERE square[3]')
+        serial_tools.send(ser, 'SETPVC square[2] Y 300')
+        serial_tools.send(ser, 'HERE square[4]')
+        serial_tools.send(ser, 'SETPVC square[4] X 200')
+        serial_tools.send(ser, 'SETPVC square[4] Y 300')
+
+        for i in range(4):
+            msg1 = 'TEACH square[{}]'.format(i)
+            serial_tools.send(ser, msg1)
+            msg2 = 'MOVE square[{}]'.format(i)
+            serial_tools.send(ser, msg2)
+            msg3 = 'HERE square[{}]'.format(i)
+            serial_tools.send(ser, msg3)
+
+        serial_tools.send(ser, 'MOVES square 1 4')
 
