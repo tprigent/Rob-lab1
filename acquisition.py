d0b87a689ee62185b7ccc5843adebc4d0f63ca99
Théo Prigent <dev@tprigent.fr>
2022-11-30 12:02:19 +0000

    Image recognition: no more need of outlines

--- acquisition.py
+++ acquisition.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def get_key_points(image_name, nb_points):
@@ -6,16 +7,12 @@ def get_key_points(image_name, nb_points):
     image = cv2.imread('input-images/{}'.format(image_name))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # detect edges
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-
     # generate keypoints
     orb = cv2.ORB_create(nb_points)
-    keypoints, des = orb.detectAndCompute(edges, None)
+    keypoints, des = orb.detectAndCompute(gray, None)
 
     # generate output image
-    img_final = cv2.drawKeypoints(edges, keypoints, None)
+    img_final = cv2.drawKeypoints(gray, keypoints, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     cv2.imwrite('output-images/keypoints-{}'.format(image_name), img_final)
 
-    # return keypoints
     return keypoints
