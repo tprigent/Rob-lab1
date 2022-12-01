2892444f32dd4189f97b4e7755536d498ca074e0
Théo Prigent <theo.prigent@tecnico.ulisboa.pt>
2022-12-01 23:56:28 +0000

    Image processing: better algorithm

--- acquisition.py
+++ acquisition.py
@@ -37,8 +37,8 @@ def analyse_image(image_name):
     for contour in enumerate(contours):
         data = contour[1]
         for i in range(len(data) - 1):
-            #if i % 10 == 0:     # downsampling
-                points.append(data[i][0])
+            # if i % 10 == 0:     # downsampling
+            points.append(data[i][0])
 
     # draw resulting points on image
     cont_image = np.zeros([width, height, 1])
@@ -73,12 +73,48 @@ def vertical_analysis(image_name):
             if edges[u, v] == 255:
                 points0.append((u, v))
 
-    for i in range(1, len(points0)-1):
-        if points0[i][0] != points0[i+1][0]:
+    for i in range(1, len(points0) - 1):
+        if points0[i][0] != points0[i + 1][0]:
             res_im0[points0[i][0], points0[i][1]] = 0
-        if points0[i-1][0] != points0[i][0]:
+        if points0[i - 1][0] != points0[i][0]:
             res_im1[points0[i][0], points0[i][1]] = 0
 
     cv2.imwrite('output-images/keypoints-0-{}'.format(image_name), res_im0)
     cv2.imwrite('output-images/keypoints-1-{}'.format(image_name), res_im1)
 
+
+def processing(image_name):
+    img = cv2.imread('input-images/{}'.format(image_name))
+
+    # get image shape
+    height = img.shape[0]
+    width = img.shape[1]
+
+    # image preprocessing (grayscale, blur and binarisation)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
+
+    # introduce discontinuity
+    print('=> Getting key points')
+    for u in tqdm(range(width)):
+        for v in range(height):
+            if u == 20 or v == 20: thresh[v, u] = 0
+            if u % 200 == 0 or v % 200 == 0: thresh[v, u] = 0
+
+    # get centroids of fractions of drawing
+    connectivity = 8
+    output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
+    centroids = output[3]
+    img_final = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+    cnt = 0
+
+    # overlay centroid on image + fill point list
+    points = []
+    for c in centroids:
+        if cnt != 0:    # avoid 1st centroid
+            points.append([int(c[1]), int(c[0])])
+            img_final[int(c[1]) - 8: int(c[1]) + 16, int(c[0]) - 8: int(c[0]) + 16] = [0, 255, 0]
+        else: cnt += 1
+    # write final image
+    cv2.imwrite('output-images/keypoints-{}'.format(image_name), img_final)
